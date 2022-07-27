bba85f0f66fa1883c9af064cc384edb5460cd72e
snowome <wjfbusy@163.com>
2022-07-28 02:44:34 +0800

    7、fabric自定义着色器设置材质

---
+++ base/src/doc/0、shader_doc.js
@@ -0,0 +1,23 @@
+var doc = `
+一、着色器定义文档
+https://cesium.com/downloads/cesiumjs/releases/b28/Documentation/
+1、czm_materialInput
+s                   float   一维UV
+st                  vec2    二维UV
+str                 vec3    三维UV
+normalEC            vec3    法线
+tangentToEyeMatrix  mat3    视图矩阵
+positionToEyeEC     vec3     fragment 到视图（相机）的距离
+
+
+
+
+
+
+
+
+
+
+
+
+`
--- base/src/doc/7、fabric自定义着色器设置材质.js
+++ base/src/doc/7、fabric自定义着色器设置材质.js
@@ -1,61 +1,5 @@
 import * as Cesium from 'cesium'
 import Gsap from 'gsap'
-import '@/_assets/cesium/Widgets/widgets.css'
-
-import '@/css/common.scss'
-
-import {CESIUM_TOKEN} from '@/config.js'
-
-Cesium.Ion.defaultAccessToken = CESIUM_TOKEN
-
-window.CESIUM_BASE_URL = './_assets/cesium/'
-
-// 设置相机默认视角
-Cesium.Camera.DEFAULT_VIEW_RECTANGLE = Cesium.Rectangle.fromDegrees(
-    // 西经
-    73.33,
-    // 南纬
-    3.51,
-    // 东经
-    135.05,
-    // 北纬
-    53.33,
-)
-
-const viewer = new Cesium.Viewer(document.body, {
-    // 信息框 是否显示
-    infoBox: false,
-    // 搜索框 是否显示
-    geocoder: false,
-    // Home 是否显示
-    homeButton: false,
-    // 2d、3d、2.5d 显示模式 是否显示
-    sceneModePicker: false,
-    // 地图影像 切换 是否显示
-    baseLayerPicker: false,
-    // 帮助按钮 是否显示
-    navigationHelpButton: false,
-    // 左下角 动画控制 是否显示
-    animation: false,
-    // 底部 时间轴 是否显示
-    timeline: false,
-    // 全屏按钮 是否显示
-    fullscreenButton: false,
-    // 天地图 https://www.tianditu.gov.cn/
-    // imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-    // 矢量地图
-    // url: "http://t0.tianditu.com/vec_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=vec&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=30d07720fa76f07732d83c748bb84211",
-    // 影像地图
-    // url: "http://t0.tianditu.com/img_w/wmts?service=wmts&request=GetTile&version=1.0.0&LAYER=img&tileMatrixSet=w&TileMatrix={TileMatrix}&TileRow={TileRow}&TileCol={TileCol}&style=default&format=tiles&tk=30d07720fa76f07732d83c748bb84211",
-    // layer: "tdtBasicLayer",
-    // style: "default",
-    // format: "image/jpeg",
-    // tileMatrixSetID: "GoogleMapsCompatible",
-    // }),
-})
-
-// 去掉左下角的logo
-viewer.cesiumWidget.creditContainer.style.display = 'none'
 
 
 // 第一步：创建几何体
@@ -139,9 +83,8 @@ Gsap.to(material.uniforms, {
 const appearance = new Cesium.EllipsoidSurfaceAppearance({
     material: material
 })
-// console.log(appearance)
-// console.log(appearance.vertexShaderSource)
-// console.log(appearance.fragmentShaderSource)
+// console.log(appearance._vertexShaderSource)
+// console.log(appearance._fragmentShaderSource)
 
 
 // 第四步：创建图元
